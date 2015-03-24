4c0ef5efe11970e2abb279b1c0467dc3597a1186
Ross Harrison <rtharrison86@gmail.com>
2015-03-24 18:39:01 -0500

    build makes the project with hiberlite and the database opens

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-cd `dirname $0`/build; cmake .. && make
+cd `dirname $0` && git submodule update --init --recursive
+
+# build hiberlite
+cd `dirname $0`/lib/hiberlite; make libhiberlite.a && cd ../../
+
+# make into build directory
+mkdir -p `dirname $0`/.build
+cd `dirname $0`/.build && cmake .. && make && cd ../
+
+# move executable to the root
+cd `dirname $0` && mv `dirname $0`/.build/ebook `dirname $0`
