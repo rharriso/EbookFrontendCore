8e5f1b22bc878c3d9ad2b175a4b7b470ca8aed88
Ross Harrison <rtharrison86@gmail.com>
2015-03-24 22:48:50 -0500

    use cmake not submodule

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
-cd `dirname $0` && git submodule update --init --recursive
-
-# build hiberlite
-cd `dirname $0`/lib/hiberlite; make libhiberlite.a && cd ../../
-
 # make into build directory
 mkdir -p `dirname $0`/.build
 cd `dirname $0`/.build && cmake .. && make && cd ../
