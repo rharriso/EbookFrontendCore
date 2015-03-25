5a438a0e2e46c5599aa83c7e62d5106cdad3ea19
Ross Harrison <rtharrison86@gmail.com>
2015-03-24 23:23:38 -0500

    print out the items stored

--- src/ebook.cpp
+++ src/ebook.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
+#include <string>
+
 #include "hiberlite.h"
 #include "models/Book.h"
 
 using namespace std;
 
 int main(){
-    Book b;
-    b.id = 10;
-
     hiberlite::Database db;
     db.open("database.name.ok.db");
+    db.registerBeanClass<Book>();
+
+
+    auto book = db.createBean<Book>();
+    cout << endl << "Enter a book title: " << endl;
+    string bookTitle;
+    cin >> bookTitle;
+    book->title = bookTitle;
+
+    cout << "Book saved: ( " << book.get_id()
+     << " ) " << book->title;  
+
+    auto books = db.getAllBeans<Book>();
+    cout << books.size() << " Books stored: " << endl;
+    for ( auto b : books){
+        cout << b.get_id() << " " << b->title << endl;
+    }
 }
--- src/models/Book.h
+++ src/models/Book.h
@@ -1,10 +1,22 @@
 #ifndef BOOK_H
 #define BOOK_H
 
+#include <string>
+
+#include "hiberlite.h"
+
 class Book
 {
+    friend class hiberlite::access;
+    template<class Archive>
+    void hibernate(Archive & ar)
+    {
+        ar & HIBERLITE_NVP(title);
+    }
+
     public:
-        int id;
+        std::string title;
 };
 
+HIBERLITE_EXPORT_CLASS(Book)
 #endif
